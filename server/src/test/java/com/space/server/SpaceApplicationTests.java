1f802ea1eccc8f8b17d70137e4f48ed99616952d
skytner <jolyskytner@gmail.com>
2025-02-11 09:12:59 +0500

    chore: init server app

---
+++ server/src/test/java/com/space/server/SpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
