d0b3d0562b3ff926e14172b55ab5033afec823ca
skytner <jolyskytner@gmail.com>
2026-02-28 16:28:35 +0400

    chore(space-1): implement declaration for react-internal eslint and created entry for ui-package

---
+++ packages/eslint-config/declarations.d.ts
@@ -0,0 +1,6 @@
+// packages/eslint-config/declarations.d.ts
+declare module '@repo/eslint-config/react-internal' {
+  import { Linter } from 'eslint';
+  const config: Linter.Config;
+  export default config;
+}
