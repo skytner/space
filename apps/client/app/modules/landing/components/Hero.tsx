0e5a00123c84bf6df0d3cca889bd6ff17841847d
sssskyytner <jolyskytner@gmail.com>
2026-03-01 02:44:49 +0400

    style(app): formatted code after biome

--- apps/client/app/modules/landing/components/Hero.tsx
+++ apps/client/app/modules/landing/components/Hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { motion } from "framer-motion";
 import { Typography } from "@packages/react-ui";
+import { motion } from "framer-motion";
 
 interface HeroProps {
 	title: string;
--- apps/client/app/modules/landing/view/LandingView.tsx
+++ apps/client/app/modules/landing/view/LandingView.tsx
@@ -1,5 +1,5 @@
-import Hero from "../components/Hero";
 import landingContent from "public/contents/landing.json";
+import Hero from "../components/Hero";
 
 export default function LandingView() {
 	const { title, description, tagline } = landingContent;
